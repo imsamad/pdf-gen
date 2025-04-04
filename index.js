e69b89b6e1f1176732fc020879676b3d088b93df
Abdus Samad <73896798+imsamad@users.noreply.github.com>
2025-04-05 00:48:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import fs from "fs";
 import { generatePdf } from "./generatePdf.js";
 dotenv.config();
 const app = express();
@@ -16,13 +17,21 @@ app.get("/generate-pdf/:slug", async (req, res) => {
     if (!filePath) {
       return res.status(500).json({ message: "Failed to generate PDF" });
     }
-
-    res.download(filePath, "document.pdf", (err) => {
+ // Check if file exists before downloading
+    fs.access(filePath, fs.constants.F_OK, (err) => {
       if (err) {
-        console.error("Error sending file:", err);
-        res.status(500).json({ message: "Failed to send file" });
+        console.error("File not found:", err);
+        return res.status(500).json({ message: "Generated PDF not found" });
       }
+
+      res.download(filePath, "document.pdf", (err) => {
+        if (err) {
+          console.error("Error sending file:", err);
+          res.status(500).json({ message: "Failed to send file" });
+        }
+      });
     });
+     
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ message: "Please try again!" });
